0653a873ad8442e2b9340a45fbbace2f67cc4070
Jongmin Kim <kdzlvaids@gmail.com>
2014-11-22 18:39:25 +0900

    Update

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 	<head>
 		<meta name="viewport" content="width=device-width">
 		<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 		<title>맘스터치 위치 찾기</title>
 		<style type="text/css">
-			html, body, #map-canvas { height: 90%; margin: 0; padding: 0;}
+			html, body { height: 95%; margin: 0; padding: 0;}
+			#header { height: 5%; margin: 0; padding: 0;}
+			#map-canvas { height: 70%; margin: 0; padding: 0;}
+			#footer { height: 20%; margin: 0; padding: 0;}
 			p {
 				font-family: "NanumGothic", sans-serif;
 				text-align:center;
@@ -30,7 +33,7 @@
 			</p>
 		</div>
 		<div id="map-canvas"></div>
-		<div class="body">
+		<div class="footer">
 			<p>
 				<button type="button" class="btn btn-primary" onclick="geofind()">내 위치</button>
 			</p>
@@ -49,16 +52,14 @@
 					<button type="button" class="btn" data-color="default">자전거도로</button>
 				</span>
 			</p>
-		</div>
-		<div class="footer">
 			<p>
 				Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://github.com/kdzlvaids/momstouch" rel="dct:source">http://github.com/kdzlvaids/momstouch</a>.<br>
 				<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">맘스터치 위치 찾기</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="mailto:kdzlvaids@gmail.com" property="cc:attributionName" rel="cc:attributionURL">Jongmin Kim</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
 			</p>
 			<p><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/80x15.png"></a></p>
 		</div>
-		<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-		<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="//apis.daum.net/maps/maps3.js?apikey=5377a73a7960f3096e68395f8a8d9c70419475d8"></script>
 		<script type="text/javascript" src="js/api/initialise.js"></script>
 		<script type="text/javascript" src="js/data/database.js"></script>
